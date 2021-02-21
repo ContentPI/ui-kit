4500fb3506c95e251c50af0286d2795cd20104af
FoggDev <foggdev@gmail.com>
2021-02-21 11:44:48 -0800

    Adding Palette types

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,56 +1,7 @@
 import { Breakpoints } from '../theme/breakpoints'
 import { FontWeight } from './fontWeight'
 import { FontSize } from './fontSize'
-
-type ColorIntensity = {
-  a: string
-  b: string
-  c: string
-  d?: string
-  e?: string
-  f?: string
-  g?: string
-  h?: string
-  i?: string
-  j?: string
-  k?: string
-  l?: string
-}
-
-type ButtonColor = {
-  color: string
-  bg: string
-  bc: string
-  hover: string
-  outlined: {
-    color: string
-    bg: string
-    bc: string
-    hover: string
-  }
-}
-
-export type Palette = {
-  white: string
-  black: string
-  blue: ColorIntensity
-  gray: ColorIntensity
-  green: ColorIntensity
-  orange: ColorIntensity
-  red: ColorIntensity
-}
-
-export type ButtonPalette = {
-  danger: ButtonColor
-  dark: ButtonColor
-  info: ButtonColor
-  light: ButtonColor
-  link: ButtonColor
-  primary: ButtonColor
-  secondary: ButtonColor
-  success: ButtonColor
-  warning: ButtonColor
-}
+import { ButtonPalette } from './palette'
 
 export type Style = 'light' | 'dark'
 
@@ -64,10 +15,10 @@ export interface Theme {
   }
   style: {
     light: {
-      btn: any
+      btn: ButtonPalette
     }
     dark: {
-      btn: any
+      btn: ButtonPalette
     }
   }
 }
