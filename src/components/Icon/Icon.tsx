60b0939db6941476d94068b38af7a330aa9d4062
FoggDev <foggdev@gmail.com>
2021-03-08 23:43:23 -0800

    Implementing Theming System - Part 2

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,23 +1,36 @@
 // Dependencies
 import React, { FC, ReactElement } from 'react'
+import { cx } from '@contentpi/utils'
 
 interface Props {
   children?: ReactElement | string
   className?: string
   type?: string
   title?: string
+  library?: 'fontawesome' | 'feather' | 'material'
+  fill?: string
+  stroke?: string
+  width?: number
+  height?: number
   background?: string
   onClick?(): void
 }
 
 const Icon: FC<Props> = props => {
-  const { type, className = '' } = props
+  const { type, className = '', children, library = 'fontawesome', width = 24 } = props
+  const height = props.height !== width ? width : 24
   const iconProps = { ...props }
 
   delete iconProps.type
   delete iconProps.className
+  delete iconProps.library
 
   let style = {}
+
+  if (library === 'fontawesome') {
+    delete iconProps.width
+    delete iconProps.height
+  }
 
   if (props.onClick) {
     style = {
@@ -25,7 +38,23 @@ const Icon: FC<Props> = props => {
     }
   }
 
-  return <i style={style} {...iconProps} className={`Icon ${type} ${className}`} />
+  if (library !== 'fontawesome') {
+    const CustomIcon = require(`./icons/${library}/${type}.svg`).default
+    iconProps.width = width
+    iconProps.height = height
+
+    return <CustomIcon {...iconProps} />
+  }
+
+  if (children) {
+    return (
+      <i style={style} {...iconProps} className={cx('Icon', className)}>
+        {children}
+      </i>
+    )
+  }
+
+  return <i style={style} {...iconProps} className={cx('Icon', `${type} ${className}`)} />
 }
 
 export default Icon
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export { default as Button } from './components/Button'
 export { default as Icon } from './components/Icon'
+export { default as Spinner } from './components/Spinner'
