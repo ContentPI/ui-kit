5097ca16865e4882b03b34349dca521dee08a6ff
FoggDev <foggdev@gmail.com>
2021-05-01 01:25:32 -0700

    Removing old App and data

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 // Components
-// import App from './App'
-import PreviewApp from './PreviewApp'
+import App from './App'
 
-// render(<App />, document.querySelector('#root'))
-render(<PreviewApp />, document.querySelector('#root'))
+render(<App />, document.querySelector('#root'))
