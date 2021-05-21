1d19be948119b736695b8a6875eed213d7a6cbd2
FoggDev <foggdev@gmail.com>
2021-05-20 20:42:10 -0700

    feat: Fixing versioning on dev

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@ lastCommit=`git log -1 --pretty=oneline`
 fix='fix'
 feat='feat'
 major='major'
-push=`git push origin main`
 
 if [[ "$lastCommit" == *"$feat"* ]]; then
   execute=`npm version minor`
   echo "Creating a feature version: $execute"
+  push=`git push origin main`
   echo "$push"
   exit
 fi
@@ -15,6 +15,7 @@ fi
 if [[ "$lastCommit" == *"$major"* ]]; then
   execute=`npm version major`
   echo "Creating a major version: $execute"
+  push=`git push origin main`
   echo "$push"
   exit
 fi
@@ -22,5 +23,6 @@ fi
 
 execute=`npm version patch`
 echo "Creating a fix version: $execute"
+push=`git push origin main`
 echo "$push"
 exit
