3e53957f5fc8c7a044acebba3fd4335edc4f962d
FoggDev <foggdev@gmail.com>
2021-06-08 17:14:58 -0700

    fix(Webpack): Adding externals

--- webpack.common.ts
+++ webpack.common.ts
@@ -54,6 +54,20 @@ const webpackConfig: any = {
       '@types': resolve(__dirname, 'src', 'types')
     },
     fallback: { crypto: false }
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
   }
 }
 
