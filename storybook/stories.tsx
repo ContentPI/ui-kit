3f1ecd4e941933c93c6476ee2f9989e7f19fb20e
OmarRoBra <50344283+OmarRoBra@users.noreply.github.com>
2021-06-25 11:48:22 -0500

    TextArea stories

--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -1,11 +1,13 @@
 import ButtonStories from '../src/components/Button/Button.stories'
 import InputStories from '../src/components/Input/Input.stories'
 import PaginationStories from '../src/components/Pagination/Pagination.stories'
+import TextAreaStories from '../src/components/TextArea/TextArea.stories'
 
 const stories: any = {
   button: ButtonStories,
   input: InputStories,
-  pagination: PaginationStories
+  pagination: PaginationStories,
+  textarea: TextAreaStories
 }
 
 export default stories
