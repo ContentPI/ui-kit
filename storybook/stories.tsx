3a5961b4e08daadf1f90250c2d444b52e977219c
OmarRoBra <50344283+OmarRoBra@users.noreply.github.com>
2021-07-01 15:38:00 -0500

    Fixing props table

--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -8,15 +8,14 @@ import TextFieldStories from '../src/components/TextField/TextField.stories'
 import BadgeStories from '../src/components/Badge/Badge.stories'
 
 const stories: any = {
-  bagde: BadgeStories,
+  badge: BadgeStories,
   button: ButtonStories,
   input: InputStories,
   pagination: PaginationStories,
   renderif: RenderIfStories,
   textarea: TextAreaStories,
   text: Text,
-  textfield: TextFieldStories,
-  bagde: BadgeStories
+  textfield: TextFieldStories
 }
 
 export default stories
