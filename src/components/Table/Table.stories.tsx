ae624067791ba0c30be47b7c6af6b24d3efa5943
FoggDev <foggdev@gmail.com>
2021-07-09 21:29:42 -0700

    fix: Fixing linter

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -8,21 +8,25 @@ const stories = {
       name: 'data',
       type: 'Array',
       default: '',
-      description: 'The content of the component',
+      description: 'The content of the component'
     }
   ],
   stories: [
     {
       name: 'Table',
       description: 'Simple Table',
-      render: <Table data={{
-        columns: ['#', 'Nombre', 'Precio'],
-        rows: [
-          ['1', 'Galaxy Z Fold2', '$1449'],
-          ['2', 'Galaxy Z Flip', '$1099'],
-          ['3', 'Galaxy Z Fold', '$1980'],
-        ]
-      }} />,
+      render: (
+        <Table
+          data={{
+            columns: ['#', 'Nombre', 'Precio'],
+            rows: [
+              ['1', 'Galaxy Z Fold2', '$1449'],
+              ['2', 'Galaxy Z Flip', '$1099'],
+              ['3', 'Galaxy Z Fold', '$1980']
+            ]
+          }}
+        />
+      ),
       prop: false,
       code: `
         <Table data={{
